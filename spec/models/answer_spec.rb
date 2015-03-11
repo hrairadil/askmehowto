8f957b364475f196eb1eb607ffdc604d91fb3667
Hrair Adilkhanyan <hrairadil@gmail.com>
2015-03-11 15:35:27 +0300

    add db connection between qna

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -1,8 +1,11 @@
 require 'rails_helper'
+require 'shoulda-matchers'
 
 describe Answer do
   let(:answer) { create :answer }
 
   it { should respond_to :body }
   it { should respond_to :question_id }
+  it { should belong_to :question }
+
 end
--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,9 +1,10 @@
 require 'rails_helper'
+require 'shoulda-matchers'
 
 describe Question do
   let(:question) { create :question, :with_answers }
 
   it { should respond_to :title }
   it { should respond_to :body }
-
+  it { should have_many :answers }
 end
