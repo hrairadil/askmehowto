5e23607004928ae0e5a4e117ad8aee88d656b045
Hrair Adilkhanyan <hrairadil@gmail.com>
2015-03-25 21:26:41 +0300

    add best to the answer model

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -5,6 +5,7 @@ describe Answer do
   let(:answer) { create :answer }
 
   it { should respond_to :body }
+  it { should respond_to :best }
   it { should respond_to :question_id }
   it { should belong_to :question }
   it { should validate_presence_of :body}
