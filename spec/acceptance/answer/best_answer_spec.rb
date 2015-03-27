24889f1cafddaf769cdf09e0a2c20ac24c51bacb
Hrair Adilkhanyan <hrairadil@gmail.com>
2015-03-27 14:44:44 +0300

    add best answer missing acceptance tests

--- spec/acceptance/answer/best_answer_spec.rb
+++ spec/acceptance/answer/best_answer_spec.rb
@@ -9,7 +9,7 @@ feature 'Choose the best answer', %q{
   given(:another_users_question) { create :question, :with_answers, user: create(:user) }
   given!(:question) { create :question, :with_all_the_best_answers, user: author }
   given!(:best_answer) { create :answer, question: question, user: author }
-
+  given!(:one_more_answer) { create :answer, question: question}
   scenario 'Unauthenticated user tries to choose the best answer' do
     visit question_path(question)
     expect(page).not_to have_link 'Best'
@@ -26,11 +26,22 @@ feature 'Choose the best answer', %q{
     end
 
     scenario 'chooses the best answer', js: true do
+
       within "#answer-#{best_answer.id}" do
         click_on 'Best'
+        expect(page).to have_selector '#best-answer'
       end
-      expect(page).to have_selector '#best-answer', count: 1
+
+      within "#answer-#{one_more_answer.id}" do
+        click_on 'Best'
+        expect(page).to have_selector '#best-answer'
+      end
+
+      expect(page.find("#answer-#{best_answer.id}")).not_to have_selector '#best-answer'
+      expect(page.find('.answers').first('div')).to have_selector '#best-answer'
     end
+
+
 
     scenario "can not choose another user's " do
       visit question_path(another_users_question)
