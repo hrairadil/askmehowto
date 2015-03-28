10006ff1e3f5a04c87e0d53c000161a36154888d
Hrair Adilkhanyan <hrairadil@gmail.com>
2015-03-28 08:06:34 +0300

    Question accepts nested attributes for attachments

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,7 +2,10 @@ class Question < ActiveRecord::Base
   belongs_to :user
   has_many :answers, dependent: :destroy
   has_many :attachments, dependent: :destroy
+
   validates :title, presence: true
   validates :body, presence: true
   validates :user_id, presence: true
+
+  accepts_nested_attributes_for :attachments
 end
--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -3,14 +3,17 @@ require 'shoulda-matchers'
 
 describe Question do
   let(:question) { create :question }
+  it { should belong_to :user }
   it { should have_many(:answers).dependent(:destroy) }
   it { should have_many(:attachments).dependent(:destroy) }
 
+  it { should respond_to :user_id }
   it { should respond_to :title }
   it { should respond_to :body }
+
   it { should validate_presence_of :title }
   it { should validate_presence_of :body }
-  it { should belong_to :user }
-  it { should respond_to :user_id }
   it { should validate_presence_of :user_id }
+
+  it { should accept_nested_attributes_for :attachments }
 end
