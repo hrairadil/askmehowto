dafa916e381d2034746bdce0e08af5f1ae529ff0
Hrair Adilkhanyan <hrairadil@gmail.com>
2015-03-29 10:00:52 +0300

    user can add more files when asks a question

--- spec/acceptance/answer/add_file_to_answer_spec.rb
+++ spec/acceptance/answer/add_file_to_answer_spec.rb
@@ -16,10 +16,10 @@ feature 'Add files to answer', %q{
 
   scenario 'User adds file when answers the question', js: true do
     fill_in 'create-answer-body', with: 'This is the best answer ever!'
-    attach_file 'File', "#{Rails.root}/spec/fixtures/screenshot.jpg"
+    attach_file 'File', "#{Rails.root}/spec/fixtures/screenshot0.jpg"
     click_on 'Create answer'
     within '.answers' do
-      expect(page).to have_link 'screenshot.jpg', href: '/uploads/attachment/file/1/screenshot.jpg'
+      expect(page).to have_link 'screenshot0.jpg', href: '/uploads/attachment/file/1/screenshot0.jpg'
     end
   end
 end
--- spec/acceptance/question/add_files_to_question_spec.rb
+++ spec/acceptance/question/add_files_to_question_spec.rb
@@ -11,15 +11,31 @@ feature 'Add files to question', %q{
   background do
     sign_in(user)
     visit questions_path
+    click_on 'Ask question'
   end
 
   scenario 'User adds file when asks question' do
-    click_on 'Ask question'
     fill_in 'Title', with: 'Test Title'
     fill_in 'Body', with: 'Test Body'
-    attach_file 'File', "#{Rails.root}/spec/fixtures/screenshot.jpg"
+    attach_file 'File', "#{Rails.root}/spec/fixtures/screenshot0.jpg"
     click_on 'Create'
 
-    expect(page).to have_link 'screenshot.jpg', href: '/uploads/attachment/file/1/screenshot.jpg'
+    expect(page).to have_link 'screenshot0.jpg', href: '/uploads/attachment/file/1/screenshot0.jpg'
+  end
+
+  scenario 'User sees link "Add more"' do
+    expect(page).to have_link "Add more"
+  end
+
+  scenario 'User adds several files when asks question', js: true do
+    fill_in 'Title', with: 'Test Title'
+    fill_in 'Body', with: 'Test Body'
+
+    click_on 'Add more'
+    inputs = all('input[type="file"]')
+    2.times { |i| inputs[i].set("#{Rails.root}/spec/fixtures/screenshot#{i}.jpg") }
+    click_on 'Create'
+
+    2.times { |i| expect(page).to have_link "screenshot#{i}.jpg" }
   end
 end
