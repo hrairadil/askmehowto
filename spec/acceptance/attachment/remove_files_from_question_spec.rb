be1c8daad0f13c93395e7ebd09edca8d9b7bd276
Hrair Adilkhanyan <hrairadil@gmail.com>
2015-03-30 20:41:50 +0300

    only author can delete attachments of the question

--- spec/acceptance/attachment/remove_files_from_question_spec.rb
+++ spec/acceptance/attachment/remove_files_from_question_spec.rb
@@ -5,20 +5,25 @@ feature 'Remove files from question', %q{
   As an author of a question
   I'd like to be able to remove files from the question
 } do
-  given!(:author){ create :user }
-  given!(:question){ create :question, :with_attachments, user: author }
+  given!(:author) { create :user }
+  given!(:user) { create :user }
+  given!(:question) { create :question, :with_attachments, user: author }
+  given!(:another_question) { create :question, :with_attachments, user: user }
   given!(:attachment) { question.attachments.first }
+  given!(:another_users_attachment) { another_question.attachments.first }
+
 
   background do
     sign_in(author)
-    visit question_path(question)
   end
 
   scenario 'Author sees remove link' do
+    visit question_path(question)
     expect(page).to have_link 'Delete file'
   end
 
   scenario 'Author removes files', js: true do
+    visit question_path(question)
     within "#file-#{attachment.id}" do
       click_on 'Delete file'
     end
@@ -26,5 +31,10 @@ feature 'Remove files from question', %q{
     expect(current_path).to eq question_path(question)
   end
 
-  scenario "Author can not remove another user's files"
+  scenario "Author can not remove another user's files", js: true do
+    visit question_path(another_question)
+    within '.question' do
+      expect(page).not_to have_link 'Delete file'
+    end
+  end
 end
