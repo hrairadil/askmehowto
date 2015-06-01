1a8c46484cc3172b703fa493c32c2ae2242151b6
Hrair Adilkhanyan <hrairadil@gmail.com>
2015-06-01 20:43:56 +0300

    add daily mailer test

--- spec/mailers/daily_mailer_spec.rb
+++ spec/mailers/daily_mailer_spec.rb
@@ -4,7 +4,10 @@ RSpec.describe DailyMailer, type: :mailer do
   describe 'digest' do
     let(:users) { create_list :user, 3 }
     let(:user) { users.first }
-    let(:mail) { DailyMailer.digest(user) }
+    let!(:question) { create :question, user: user }
+    let(:mail) { DailyMailer.digest(user).deliver_now }
+
+    before { question.update(created_at: question.created_at.to_date - 1) }
 
     it 'renders the headers' do
       expect(mail.subject).to eq('Digest')
@@ -14,6 +17,7 @@ RSpec.describe DailyMailer, type: :mailer do
 
     it 'renders the body' do
       expect(mail.body.encoded).to match('Hi')
+      expect(mail.body.encoded).to have_content question.title
     end
 
     describe '.send_daily_digest' do
