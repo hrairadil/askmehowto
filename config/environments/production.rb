e5f58b21eb9ddc3f38f00af804709d9b9b879646
Hrair Adilkhanyan <hrairadil@gmail.com>
2015-06-09 21:16:26 +0300

    add host ip to production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,5 +77,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { :host => "http://askmehow.to" }
+  config.action_mailer.default_url_options = { host: '52.28.135.241' }
 end
