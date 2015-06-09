538ef24e613a3f746219ea1b43e930e8a9e22093
Hrair Adilkhanyan <hrairadil@gmail.com>
2015-06-09 23:20:17 +0300

    add capfile deploy.rb production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,5 +78,4 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.default_url_options = { host: 'http://52.28.135.241' }
-  config.action_mailer.delivery_method = :smtp
 end
